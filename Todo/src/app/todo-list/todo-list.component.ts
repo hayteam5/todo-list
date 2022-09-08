6b0b813e8e3def4db23c8925bb04e93115dcda24
hayteam5 <hayteam797@gmail.com>
2022-09-08 18:04:11 +0200

    change string array to object array

--- Todo/src/app/todo-list/todo-list.component.ts
+++ Todo/src/app/todo-list/todo-list.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+export interface Todo {
+  id: number,
+  done: boolean,
+  title: string,
+  favorite: boolean,
+}
 
 @Component({
   selector: 'app-todo-list',
@@ -7,14 +13,21 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
-  @Input() todo: string = "";
+  @Input() title: string = "";
   constructor() { }
 
-  todos: string[] = []
+  todos: Todo[] = []
   isChecked : boolean = false
+  id?: number
+  favorite: boolean = false
+  todo?: Todo
+
+
 
   addNewTodo(): void {
-    this.todos.push(this.todo)
+      this.todo?.title = this.title
+      this.todos.push(this.todo)
+    }
     console.log(this.todos)
   }
 
